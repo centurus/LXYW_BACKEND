ce83c2403da9681aad3717c51b34570f1141e3fc
LXYW <1198966503@qq.com>
2018-04-26 21:22:41 +0800

    Spring-Boot-Maven

---
+++ src/main/java/com/lxyw/bakcend/BakcendApplication.java
@@ -0,0 +1,12 @@
+package com.lxyw.bakcend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakcendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakcendApplication.class, args);
+    }
+}
