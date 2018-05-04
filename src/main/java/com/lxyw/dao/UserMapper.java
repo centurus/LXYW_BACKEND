f0e741c02ac8be6da22b03cde2e4217498cc402e
“wanghao” <wanghao1@edianzu.com>
2018-05-04 15:51:34 +0800

    mybatis-generator batchInsert

--- src/main/java/com/lxyw/dao/UserMapper.java
+++ src/main/java/com/lxyw/dao/UserMapper.java
@@ -1,7 +1,7 @@
 package com.lxyw.dao;
 
-
 import com.lxyw.entity.User;
+import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper {
     int deleteByPrimaryKey(Long id);
@@ -15,4 +15,13 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+    /**
+     * 这是Mybatis Generator拓展插件生成的方法(请勿删除).
+     * This method corresponds to the database table user
+     *
+     * @mbg.generated
+     * @author hewei
+     */
+    int batchInsert(@Param("list") java.util.List<User> list);
 }
