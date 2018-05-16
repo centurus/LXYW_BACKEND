dc28c72e316564cbcea5420b68081a6ec070ce8b
“centauruswang” <1198966503@qq.com>
2018-05-16 11:07:28 +0800

    log测试

--- src/test/java/com/lxyw/service/UserInfoServiceImplTest.java
+++ src/test/java/com/lxyw/service/UserInfoServiceImplTest.java
@@ -3,6 +3,8 @@ package com.lxyw.service;
 import com.lxyw.entity.UserInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,6 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserInfoServiceImplTest {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private UserInfoService userInfoService;
@@ -20,5 +23,7 @@ public class UserInfoServiceImplTest {
         userInfo.setId("245a951a89a642459cccf51d01a4fc01");
         userInfo.setPassword("2789");
         userInfoService.modifyUserPassword(userInfo);
+        logger.info("1111");
+
     }
 }
